08c84b848743f43b2f270aef21c857c6fc8db405
liyaojian <m@liyaojian.com>
2017-09-14 17:37:54 +0800

    fix

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-#环信sdk
+环信sdk
+
 主要功能为 导出聊天记录 支持2017年3月后的最新接口
+
+## 安装
+
+安装有两种方式：
+
+### ① 直接编辑配置文件
+
+将以下内容增加到 composer.json：
+
+```json
+require: {
+    "johnlui/aliyun-oss": "0.8.*"
+}
+```
+
+然后运行 `composer update`。
+
+### ② 执行命令安装
+
+运行命令：
+
+```bash
+composer require liyaojian/easemob
+```
--- tests/test.php
+++ tests/test.php
@@ -17,9 +17,11 @@ $time = 2017091415;
 $path = ROOT . "/cache/";
 try {
     $res = $easemob->saveMessageHistory($time, $path);
+    $array = [];
     foreach ($res as $k => $v) {
-        print_r(readgz($path.$v));
+        $array = array_merge($array,readgz($path.$v));
     }
+    print_r($array);
 } catch (\liyaojian\Easemob\App\EasemobError $e) {
     echo $e->getCode().' | '.$e->getMessage().' | '.$e->getTraceAsString();
 }
